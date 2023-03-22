ae8b8fb5f5d432a224255e77c0ef73bf32b1b057
JHoon <kjhun9062@g.yjc.ac.kr>
2023-03-22 14:59:39 +0900

    whack-a-mole 주석

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,3 +1,4 @@
+// 태그 선택자
 const $timer = document.querySelector("#timer");
 const $score = document.querySelector("#score");
 const $game = document.querySelector("#game");
@@ -5,6 +6,7 @@ const $start = document.querySelector("#start");
 const $life = document.querySelector("#life");
 const $$cells = document.querySelectorAll(".cell");
 
+// 초기 설정값
 const holes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let started = false;
 let score = 0;
@@ -13,12 +15,37 @@ let bombPercent = 0.5;
 let time = 60;
 let life = 3;
 
+// 시작버튼 클릭 시 함수 호출
+$start.addEventListener("click", () => {
+  if (started) {
+    return;
+  }
+  life = 3; // 초기화
+  $life.textContent = `목숨:${life}`;
+  started = true; // 재시작 방지
+  const timerId = setInterval(() => {
+    time = (time * 10 - 1) / 10;
+    $timer.textContent = time; // 0.1초마다 시간 업데이트
+    if (time === 0) {
+      // 0초일때 정지
+      clearInterval(timerId); // 시간 업데이트 종료
+      clearInterval(tickId);
+      setTimeout(() => {
+        alert(`시간 종료! 점수는${score}점`);
+      }, 50);
+    }
+  }, 100);
+  const tickId = setInterval(tick, 1000);
+  tick();
+});
+
 function tick() {
   holes.forEach((hole, index) => {
     if (hole) {
       //이벤트 발생중이면 실행x
       return;
     }
+    // 확률로 두더지 폭탄 정하기
     if (Math.random() < gopherPercent) {
       const $gopher = $$cells[index].querySelector(".gopher");
       holes[index] = setTimeout(() => {
@@ -38,25 +65,31 @@ function tick() {
     }
   });
 }
+
+// 이벤트 함수 심기
 $$cells.forEach(($cell, index) => {
   $cell.querySelector(".gopher").addEventListener("click", (event) => {
+    // 이미 클릭되었는지 검사
     if (!event.target.classList.contains("dead")) {
       score += 1;
       $score.textContent = score;
     }
-    event.target.classList.add("dead");
-    event.target.classList.add("hidden");
-    clearTimeout(holes[index]);
+    event.target.classList.add("dead"); // 죽음표시
+    event.target.classList.add("hidden"); // 다시 숨기기
+    clearTimeout(holes[index]); // 기존 타이머 제거
     setTimeout(() => {
+      // 초기화
       holes[index] = 0;
       event.target.classList.remove("dead");
     }, 1000);
   });
+
   $cell.querySelector(".bomb").addEventListener("click", (event) => {
     if (!event.target.classList.contains("boom")) {
       life -= 1;
       $life.textContent = `목숨:${life}`;
       if (life === 0) {
+        // 폭탄을 세번 누를시
         setTimeout(() => {
           alert(`Dead! 점수는${score}점`);
         }, 50);
@@ -70,25 +103,4 @@ $$cells.forEach(($cell, index) => {
       event.target.classList.remove("boom");
     }, 1000);
   });
-});
-$start.addEventListener("click", () => {
-  if (started) {
-    return;
-  }
-  life = 3;
-  $life.textContent = `목숨:${life}`;
-  started = true;
-  const timerId = setInterval(() => {
-    time = (time * 10 - 1) / 10;
-    $timer.textContent = time;
-    if (time === 0) {
-      clearInterval(timerId);
-      clearInterval(tickId);
-      setTimeout(() => {
-        alert(`시간 종료! 점수는${score}점`);
-      }, 50);
-    }
-  }, 100);
-  const tickId = setInterval(tick, 1000);
-  tick();
 });
